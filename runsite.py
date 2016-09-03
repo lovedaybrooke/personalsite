4a400776b73e06f764cde99ca4fba8ae71f9b5e9
Kat Matfield <katmatfield@gmail.com>
2016-09-03 13:47:36 +0100

    Tidy up python file

--- runsite.py
+++ runsite.py
@@ -3,21 +3,26 @@ from flask import render_template, redirect
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/<page>')
 def section(page=None):
     if page in shortcuts.keys():
-    	return redirect(shortcuts[page], code=302)
+        return redirect(shortcuts[page], code=302)
     else:
-	    return render_template('{0}.html'.format(page), page=page)
+        return render_template('{0}.html'.format(page), page=page)
+
 
 @app.route('/writing/<titleslug>')
 def blog(titleslug=None):
-    return render_template('/writing/{0}.html'.format(titleslug), page="writing")
+    return render_template('/writing/{0}.html'.format(titleslug),
+        page="writing")
+
 
 shortcuts = {
-		"research-irrational-world": "/writing/research-irrational-world"
-	}
+        "research-irrational-world": "/writing/research-irrational-world"
+    }
